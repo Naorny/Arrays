b56ab60feeba4495d0ddc17d704911e7a788efb2
Naorny <huliaievaelena@gmail.com>
2024-06-11 15:48:54 +0300

    Maximum among an array of 20 numbers

---
+++ src/main/java/org/arrays/Main.java
@@ -0,0 +1,33 @@
+package org.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        max(initializeArray());
+    }
+        public static int[] initializeArray() {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[20];
+
+        System.out.println("Enter elements of Array:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(scan.next());
+        }
+        System.out.println("Array of elements: " + Arrays.toString(array) + " ");
+        return array;
+    }
+
+        static void max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        System.out.println("Max element is :" + max);
+    }
+}
+
+
