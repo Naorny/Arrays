3506ddae0ebc4aae8d5172ef34b1025dca96c7f5
Naorny <huliaievaelena@gmail.com>
2024-06-12 16:11:22 +0300

    One large array and two small arrays

---
+++ src/main/java/org/arrays/OneLargeTwoSmall.java
@@ -0,0 +1,47 @@
+package org.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class OneLargeTwoSmall {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] large_array = new String[20];
+        System.out.println("Enter 20 elements of Large Array:");
+        for (int i = 0; i < large_array.length; i++) {
+            large_array[i] = scan.nextLine();
+        }
+        System.out.println("Large array of 20 elements: " + Arrays.toString(large_array) + " ");
+
+        String[] array1 = new String[10];
+        System.out.println("Enter 10 elements of the first small array:");
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = scan.nextLine();
+        }
+        System.out.println("The first small array is: " + Arrays.toString(array1) + " ");
+
+        String[] array2 = new String[10];
+        System.out.println("Enter 10 elements of the second small array:");
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = scan.nextLine();
+        }
+        System.out.println("The second small array is: " + Arrays.toString(array2) + " ");
+
+        System.arraycopy(large_array, 0, array1, 0, 10);
+        System.arraycopy(large_array, 10, array2, 0, 10);
+
+        System.out.println("The new second small array is:");
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println(array2[i]);
+        }
+    }
+}
+
+
+            // One large array and two small arrays
+            //
+            //1. Create an array for 20 numbers.
+            //2. Enter values into it from the keyboard.
+            //3. Create two arrays of 10 numbers each.
+            //4. Copy the large array into two small arrays: half of the numbers into the first small array, the other half into the second small array.
+            //5. Display the second small array on the screen, each value on a new line.
